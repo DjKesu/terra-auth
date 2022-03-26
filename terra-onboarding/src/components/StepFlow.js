38b4395da9885e979b1eb50e5399f942240ed467
DhruvBhatia0 <dhruvhead@gamil.com>
2022-03-26 14:28:48 -0400

    Updated stepflow ui

--- terra-onboarding/src/components/StepFlow.js
+++ terra-onboarding/src/components/StepFlow.js
@@ -6,11 +6,63 @@ export default function StepFlow() {
   const nextStep = () => setActive((current) => (current < 3 ? current + 1 : current));
   const prevStep = () => setActive((current) => (current > 0 ? current - 1 : current));
 
+
+  // form stuff
+  const [formData, setFormData] = useState({
+    walletName: "",
+    password: "",
+    passwordConfirm: ""
+  });
+
+  const updateFormData = event =>
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    });
+
+  const { walletName, password, passwordConfirm } = formData;
+
   return (
     <>
       <Stepper active={active} onStepClick={setActive} breakpoint="sm">
         <Stepper.Step label="Fist step" description="Create an account" allowStepSelect={active > 0}>
           Step 1 content: Create an account
+          {/* wallet name, password, sentence */}
+          <form>
+            <div className='inputBox'>
+              <input
+        value={walletName}
+        onChange={e => updateFormData(e)}
+        placeholder="Wallet Name"
+        type="text"
+        name="walletName"
+        required
+      />
+            </div>
+            <div className='inputBox'>
+              <input
+        value={password}
+        onChange={e => updateFormData(e)}
+        placeholder="Password"
+        type="password"
+        name="password"
+        required
+      />
+            </div>
+      
+      <div className='inputBox'>
+        <input
+        value={passwordConfirm}
+        onChange={e => updateFormData(e)}
+        placeholder="Confirm Password"
+        type="password"
+        name="passwordConfirm"
+        required
+      />
+      </div>
+      
+      <button type="submit" className="login-with-google-btn">SUBMIT</button>
+    </form>
         </Stepper.Step>
         <Stepper.Step label="Second step" description="Verify email" allowStepSelect={active > 1}>
           Step 2 content: Verify email
