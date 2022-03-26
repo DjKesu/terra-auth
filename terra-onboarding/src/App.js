8efe23c3db38737a14a9694189987847d7dee5ce
DjKesu <km67000@yahoo.com>
2022-03-26 18:46:36 -0400

    Removed some buttons

--- terra-onboarding/src/App.js
+++ terra-onboarding/src/App.js
@@ -1,15 +1,11 @@
 import './App.css';
 import Header from './components/Header.js';
-import StepFlow from './components/StepFlow.js';
 import ConnectWallet from './components/ConnectWallet.js';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-        {/* <button type="button" className="login-with-google-btn">Recover Terra Station Wallet</button> */}
-        <button type="button" className="login-with-google-btn">Create New Terra Station Wallet</button>
-        {/* <StepFlow/> */}
       <ConnectWallet/>
     </div>
   );
