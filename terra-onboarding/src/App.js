8ed9b8715f8a76080d3444b54865789712eab47a
George Shao <GeorgeShao123@gmail.com>
2022-03-26 13:07:40 -0400

    Added stepper

--- terra-onboarding/src/App.js
+++ terra-onboarding/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header.js';
 import BoxContent from './components/BoxContent.js';
 import ConnectSample from './components/Test.js';
+import StepFlow from './components/StepFlow.js';
 
 //hi guys! its me
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header/>
         <button type="button" className="login-with-google-btn">Recover Terra Station Wallet</button>
         <button type="button" className="login-with-google-btn">Create New Terra Station Wallet</button>
+        <StepFlow/>
       <ConnectSample/>
     </div>
   );
